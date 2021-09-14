caac3aacf4f2b27446a5e0d2b23aebca15663b71
BrenoRev <silvabreno462@gmail.com>
2021-09-14 00:36:48 -0300

    Criando o end-point de atualização

--- src/main/java/com/devrev/apirest/controller/IndexController.java
+++ src/main/java/com/devrev/apirest/controller/IndexController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +42,12 @@ public class IndexController {
 		usuarioRepository.save(usuario);
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
+	
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
+		usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+	}
+
 	
 }
