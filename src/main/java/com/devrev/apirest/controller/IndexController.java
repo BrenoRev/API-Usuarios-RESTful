c5cebbfd2494a6dee7309936ff926c93aadbc30c
BrenoRev <silvabreno462@gmail.com>
2021-09-13 22:44:21 -0300

    Testando o método init get

--- src/main/java/com/devrev/apirest/controller/IndexController.java
+++ src/main/java/com/devrev/apirest/controller/IndexController.java
@@ -3,16 +3,20 @@ package com.devrev.apirest.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/usuario")
 @RestController
 public class IndexController {
 
-	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity("Olá REST Spring Boot", HttpStatus.OK);
+	@GetMapping(value = "/{salario}" , produces = "application/json")
+	public ResponseEntity init(@RequestParam(value = "nome", defaultValue="Nome não informado", required = false) String nome,
+								@PathVariable(value = "salario") Double salario) {
+		System.out.println(nome);
+		return new ResponseEntity("Olá REST Spring Boot " + nome+ " Salario é: " + salario, HttpStatus.OK);
 	}
 	
 	
