9b31c089839e49144924941817a804ac2d68bcfe
BrenoRev <silvabreno462@gmail.com>
2021-09-16 01:39:20 -0300

    Amarrando o usuario ao telefone com o usuario_id

--- src/main/java/com/devrev/apirest/controller/IndexController.java
+++ src/main/java/com/devrev/apirest/controller/IndexController.java
@@ -39,6 +39,12 @@ public class IndexController {
 	
 	@PostMapping(value = "/", produces = "application/json")
 	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+		
+		// Amarrando a foreign key do usuario no telefone
+		for(int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+		
 		usuarioRepository.save(usuario);
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
