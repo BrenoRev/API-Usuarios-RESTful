6ca8af9605f4f01ec0d8533ae1ab1095e455efd9
BrenoRev <silvabreno462@gmail.com>
2021-09-20 23:46:35 -0300

    Deploy Heroku

--- src/main/java/com/devrev/apirest/ApiRestApplication.java
+++ src/main/java/com/devrev/apirest/ApiRestApplication.java
@@ -3,11 +3,20 @@ package com.devrev.apirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ApiRestApplication {
+@SpringBootApplication  //  implements WebMvcConfigurer
+public class ApiRestApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 	}
 
+	/* Mapeamento Global 
+	// Libera os end-points do usuario para POST PUT GET e DELETE para todos
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/usuario/**")
+		.allowedMethods("POST", "PUT", "GET", "DELETE")
+		.allowedOrigins("*");
+	}
+	*/
 }
--- src/main/java/com/devrev/apirest/controller/IndexController.java
+++ src/main/java/com/devrev/apirest/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.devrev.apirest.repository.UsuarioRepository;
 
 @RequestMapping("/usuario")
 @RestController
+@CrossOrigin(origins =  "*")
 public class IndexController {
 
 	@Autowired
