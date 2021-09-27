0eb7d567ab48e81a9e2266927e1d7f7874aeb075
BrenoRev <silvabreno462@gmail.com>
2021-09-26 23:48:29 -0300

    Spring Security - Configurando Usuário

--- src/main/java/com/devrev/apirest/model/Role.java
+++ src/main/java/com/devrev/apirest/model/Role.java
@@ -1,5 +1,44 @@
 package com.devrev.apirest.model;
 
-public class Role {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@SequenceGenerator(name = "seq_role", sequenceName = "seq_role", allocationSize = 1, initialValue = 1)
+public class Role  implements GrantedAuthority{
+	private static final long serialVersionUID = 1L;
+
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_role")
+	@Id
+	private Long id;
+	
+	private String nomeRole; /* Papel, exemplo ROLE_SECRETÁRIO ou ROLE_GERENTE */
+
+	@Override
+	public String getAuthority() { // Retorna o nome do papel, acesso ou autorização ex: ROLE_GERENTE
+		return this.nomeRole;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeRole() {
+		return nomeRole;
+	}
+
+	public void setNomeRole(String nomeRole) {
+		this.nomeRole = nomeRole;
+	}
+
 
 }
--- src/main/java/com/devrev/apirest/model/Usuario.java
+++ src/main/java/com/devrev/apirest/model/Usuario.java
@@ -1,19 +1,28 @@
 package com.devrev.apirest.model;
 
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.CascadeType;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
+import javax.persistence.UniqueConstraint;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-public class Usuario implements Serializable{
+public class Usuario implements UserDetails{
 	private static final long serialVersionUID = 1L;
 
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -26,8 +35,56 @@ public class Usuario implements Serializable{
 	
 	private String senha;
 	
-	@OneToMany(mappedBy="usuario", orphanRemoval = true, cascade = CascadeType.ALL)
+	@OneToMany(mappedBy="usuario", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<Telefone> telefones = new ArrayList<Telefone>();
+	
+	@OneToMany(fetch = FetchType.EAGER)
+	// Criar uma tabela no banco de dados com o id do usuario e o id da role que ele tem
+	@JoinTable(name = "usuarios_role", uniqueConstraints = @UniqueConstraint(
+	columnNames = {"usuario_id", "role_id"}, name="unique_role_user"), 
+	joinColumns = @JoinColumn(name = "usuario_id" , referencedColumnName = "id" , table="usuario", unique = false,
+	foreignKey = @ForeignKey (name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
+	inverseJoinColumns = @JoinColumn (name = "role_id", referencedColumnName = "id",
+	table = "role", unique = false, updatable = false,
+	foreignKey = @ForeignKey (name="role_fk" , value = ConstraintMode.CONSTRAINT)))
+	private List<Role> roles; /* Os Acessos */ 
+
+	/* Autorizações, os acessos do usuário ROLE_ADMIN ou ROLE_VISITANTE e etc.*/ 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}	
+	
 	
 	public Long getId() {
 		return id;
@@ -91,6 +148,4 @@ public class Usuario implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 
-	
-	
 }
