f524f04c00b428d983a0ad1bca3e0765a11bdcf0
BrenoRev <silvabreno462@gmail.com>
2021-09-29 22:00:33 -0300

    (JWT) - Criando a classe JwtTokenAutenticacaoService

---
+++ src/main/java/com/devrev/apirest/ApplicationContextLoad.java
@@ -0,0 +1,26 @@
+package com.devrev.apirest;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextLoad implements ApplicationContextAware{
+
+	// Deixa todo o contexto da aplicação na memória pra ser acessada sem precisar instanciar nada
+	
+	@Autowired
+	private static ApplicationContext applicationContext;
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+	
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+
+}
---
+++ src/main/java/com/devrev/apirest/security/JWTTokenAutenticacaoService.java
@@ -0,0 +1,101 @@
+package com.devrev.apirest.security;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.devrev.apirest.ApplicationContextLoad;
+import com.devrev.apirest.model.Usuario;
+import com.devrev.apirest.repository.UsuarioRepository;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+@Component
+public class JWTTokenAutenticacaoService {
+
+	// Tempo de validade do Token de 1 dia
+	private static final long EXPIRATION_TIME = 86400000;
+	
+	// Uma senha única para compor a autenticação
+	private static final String SECRET = "SenhaSecreta---*-*xs-x*s-x*-sx*-s";
+	
+	// Prefixo padrão de Token
+	private static final String TOKEN_PREFIX = "Bearer";
+	
+	// Prefixo para o header da resposta
+	private static final String HEADER_STRING = "Authorization";
+	
+	// Gerando token de autenticado e adicionando ao cabeçalho de resposta Http
+	public void addAuthentication(HttpServletResponse response, String username) throws Exception {
+	
+		// Montagem do token
+		
+						// Chama o gerador de Token
+		String JWT = Jwts.builder()
+						// Adiciona um usuário
+					.setSubject(username)
+						// Seta um limite de tempo até deslogar o usuário
+					.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+						// Adiciona um algoritmo pra criptografar a token e geração da senha
+					.signWith(SignatureAlgorithm.HS512, SECRET).compact();
+		
+		// Concatena o token com o prefixo
+		String token = TOKEN_PREFIX + " " + JWT; // Bearer SenhaSecreta---*-*xs-x*s-x*-sx*-s
+		
+		// Adiciona no cabeçalho HTTP
+		response.addHeader(HEADER_STRING, token); // Authorization: Bearer SenhaSecreta---*-*xs-x*s-x*-sx*-s
+		
+		// Escreve o token como resposta no corpo HTTP
+		response.getWriter().write("{\"Authorization\": \""+token+"\"}");
+	}
+	
+	// Retorna o usuário validado com token ou caso não seja válido retorna null
+	public Authentication getAuthentication(HttpServletRequest request) {
+		
+		// Pega o token enviado no cabeçalho HTTP
+		String token = request.getHeader(HEADER_STRING);
+		
+		// Se existir token
+		if(token != null) {
+			
+			// Faz a validação do token do usuário na requisição
+			String user = Jwts.parser()
+					// Coloca a assinatura como SenhaSecreta---*-*xs-x*s-x*-sx*-s
+					.setSigningKey(SECRET)
+					// Tira o prefixo do token
+					.parseClaimsJws(token.replace(TOKEN_PREFIX, "")) // SenhaSecreta---*-*xs-x*s-x*-sx*-s
+					// Pega o corpo da requisição
+					.getBody()
+					// Pega o objeto da requisição
+					.getSubject(); // Retorna o usuário ex: João Silva
+			
+			// Verifica se o usuario existe
+			if(user != null) {
+				
+				// Cria uma instância do usuário logado
+				Usuario usuario = ApplicationContextLoad.getApplicationContext()
+								  .getBean(UsuarioRepository.class).findUserByLogin(user);
+				
+				// Verificar se encontrou o usuário no token
+					if(usuario != null) {
+						
+						// Retorna o usuário autenticado
+						return new UsernamePasswordAuthenticationToken(usuario.getLogin(),
+																	   usuario.getSenha(),
+																	   usuario.getAuthorities()
+																	   );
+				}
+			}
+		}
+		return null; // Não existe token validado
+	}
+	
+}
