f10cac1f62ab93febbd744a901482aea948cf941
BrenoRev <silvabreno462@gmail.com>
2021-11-18 00:17:44 -0300

    Deploy Heroku2.0

--- src/main/java/com/devrev/apirest/controller/UsuarioController.java
+++ src/main/java/com/devrev/apirest/controller/UsuarioController.java
@@ -44,92 +44,90 @@ import net.sf.jasperreports.engine.JRException;
 
 @RequestMapping("/usuario")
 @RestController
-@CrossOrigin(origins =  "*")
+@CrossOrigin(origins = "*")
 @EnableCaching
 public class UsuarioController {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
-	
+
 	@Autowired
 	private ImplementacaoUserDetailsService usuarioService;
-	
+
 	@Autowired
 	private ProfissaoRepository profissaoRepository;
-	
+
 	@Autowired
 	private RelatorioService relatorioService;
-	
-	@GetMapping(value = "/{id}" , produces = "application/json")
+
+	@GetMapping(value = "/{id}", produces = "application/json")
 	public ResponseEntity<UsuarioDTO> getId(@PathVariable(value = "id") Long id) {
 		Optional<Usuario> usuario = usuarioRepository.findById(id);
 		return new ResponseEntity<UsuarioDTO>(new UsuarioDTO(usuario.get()), HttpStatus.OK);
 	}
-	
-	
-	/* Vamos Supor que o carregamento de usuario seja um processo lento
-	 e queremos controler ele com cache par agilizar o processo */
-	
+
+	/*
+	 * Vamos Supor que o carregamento de usuario seja um processo lento e queremos
+	 * controler ele com cache par agilizar o processo
+	 */
+
 	@GetMapping(value = "/", produces = "application/json")
-	
+
 	// Limpa o cache conforme necessário pra não estourar memória
-	@CacheEvict(value="allusers", allEntries = true)
+	@CacheEvict(value = "allusers", allEntries = true)
 	// Atualiza o cache quando tiver modificações
 	@CachePut("allusers")
 	public ResponseEntity<List<UsuarioDTO>> getAll() throws InterruptedException {
-		
-		List<Usuario> listUsuario= usuarioRepository.findAll();
+
+		List<Usuario> listUsuario = usuarioRepository.findAll();
 		List<UsuarioDTO> listarDTO = new ArrayList<>();
 		listUsuario.forEach((user) -> {
-			listarDTO.add(new UsuarioDTO(user.getId() ,user.getLogin(), user.getNome(), user.getCpf(), user.getDataNascimento(), user.getProfissao(), user.getSalario(), user.getEmail()));
+			listarDTO.add(new UsuarioDTO(user.getId(), user.getLogin(), user.getNome(), user.getCpf(),
+					user.getDataNascimento(), user.getProfissao(), user.getSalario(), user.getEmail()));
 		});
 		return new ResponseEntity<List<UsuarioDTO>>(listarDTO, HttpStatus.OK);
 	}
-	
-	
+
 	@GetMapping(value = "/page/{pagina}", produces = "application/json")
 	public ResponseEntity<Page<Usuario>> getAll(@PathVariable("pagina") int pagina) throws InterruptedException {
 		int size = 5;
-		
-		if(pagina  == 0) {
+
+		if (pagina == 0) {
 			size = 6;
-		}else {
+		} else {
 			size = 5;
 		}
 		PageRequest page = PageRequest.of(pagina, size, Sort.by("nome"));
 		Page<Usuario> listarDTO = usuarioRepository.findAll(page);
-		
+
 		return new ResponseEntity<Page<Usuario>>(listarDTO, HttpStatus.OK);
 	}
 
 	@PostMapping(value = "/register", produces = "application/json")
-	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
 		
-		usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
+		usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha())); 
 		usuarioRepository.save(usuario);
-		
 		usuarioService.insereAcessoPadrao(usuario.getId());
-		
-		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK); 
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
-	
+
 	@PutMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
-		
-		// Se a senha da alterção for diferente da cadastrada no banco de dados ele vai criptografar antes de salvar novamente
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+
+		// Se a senha da alterção for diferente da cadastrada no banco de dados ele vai
+		// criptografar antes de salvar novamente
 		Usuario usuarioAntigo = usuarioRepository.getById(usuario.getId());
-		if(!new BCryptPasswordEncoder().matches(usuarioAntigo.getSenha(), usuario.getSenha())) {
+		if (!new BCryptPasswordEncoder().matches(usuarioAntigo.getSenha(), usuario.getSenha())) {
 			usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
 		}
 
 		usuarioRepository.save(usuario);
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
-	
+
 	@PatchMapping(value = "/patch")
-	public ResponseEntity<Usuario> atualizarParcial(@RequestBody Usuario usuario){
-		
+	public ResponseEntity<Usuario> atualizarParcial(@RequestBody Usuario usuario) {
 		Usuario usuarioAntigo = usuarioRepository.getById(usuario.getId());
 		usuarioAntigo.setId(usuario.getId());
 		usuarioAntigo.setLogin(usuario.getLogin());
@@ -145,49 +143,52 @@ public class UsuarioController {
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
 
-	
-	@DeleteMapping(value = "/{id}" , produces = "application/json")
+	@DeleteMapping(value = "/{id}", produces = "application/json")
 	public ResponseEntity<String> deletar(@PathVariable(value = "id") Long id) {
 		usuarioRepository.deleteById(id);
-		return new ResponseEntity<String>("Usuario com o id " +id+" deletado com sucesso", HttpStatus.OK);
+		return new ResponseEntity<String>("Usuario com o id " + id + " deletado com sucesso", HttpStatus.OK);
 	}
-	
+
 	@GetMapping(value = "/name/{nome}", produces = "application/json")
-	public ResponseEntity<List<UsuarioDTO>> usuarioPorNome(@PathVariable("nome") String nome) throws InterruptedException {
-		
-		List<Usuario> listUsuario= usuarioRepository.findUserByName(nome);
+	public ResponseEntity<List<UsuarioDTO>> usuarioPorNome(@PathVariable("nome") String nome)
+			throws InterruptedException {
+
+		List<Usuario> listUsuario = usuarioRepository.findUserByName(nome);
 		List<UsuarioDTO> listarDTO = new ArrayList<>();
 		listUsuario.forEach((user) -> {
-			listarDTO.add(new UsuarioDTO(user.getId() ,user.getLogin(), user.getNome(), user.getCpf(), user.getDataNascimento(), user.getProfissao(), user.getSalario(), user.getEmail()));
+			listarDTO.add(new UsuarioDTO(user.getId(), user.getLogin(), user.getNome(), user.getCpf(),
+					user.getDataNascimento(), user.getProfissao(), user.getSalario(), user.getEmail()));
 		});
 		return new ResponseEntity<List<UsuarioDTO>>(listarDTO, HttpStatus.OK);
 	}
-	
 
-	/*END-POINT consulta de usuário por nome*/
+	/* END-POINT consulta de usuário por nome */
 	@GetMapping(value = "/name/{nome}/page/{page}", produces = "application/json")
 	@CachePut("cacheusuarios")
-	public ResponseEntity<Page<Usuario>> usuarioPorNomePage (@PathVariable("nome") String nome, @PathVariable("page") int page) throws InterruptedException{
-		
+	public ResponseEntity<Page<Usuario>> usuarioPorNomePage(@PathVariable("nome") String nome,
+			@PathVariable("page") int page) throws InterruptedException {
+
 		PageRequest pageRequest = PageRequest.of(page, 5, Sort.by("nome"));
 		List<Usuario> all = usuarioRepository.findUserByName(nome);
 		Page<Usuario> list = new PageImpl<Usuario>(all, pageRequest, 5);
-		
+
 		return new ResponseEntity<Page<Usuario>>(list, HttpStatus.OK);
-		
+
 	}
-	 
+
 	@GetMapping(value = "/relatorio/{id}", produces = "application/text")
-	public ResponseEntity<String> downloadRelatorio(@PathVariable("id") Integer id, HttpServletRequest httpServletRequest) throws SQLException, JRException{
-		
+	public ResponseEntity<String> downloadRelatorio(@PathVariable("id") Integer id,
+			HttpServletRequest httpServletRequest) throws SQLException, JRException {
+
 		// Adicionando os parametros de busca do jasper relatorios
-		Map<String,Object> param = new HashMap<String, Object>();
+		Map<String, Object> param = new HashMap<String, Object>();
 		param.put("ID", id);
-		
-		byte[] pdf = relatorioService.gerarRelatorio("relatorio-usuario", httpServletRequest.getServletContext(), param);
-		
-		String base64Pdf = "data:application/pdf;base64,"+ Base64.encodeBase64String(pdf);
-		
+
+		byte[] pdf = relatorioService.gerarRelatorio("relatorio-usuario", httpServletRequest.getServletContext(),
+				param);
+
+		String base64Pdf = "data:application/pdf;base64," + Base64.encodeBase64String(pdf);
+
 		return new ResponseEntity<String>(base64Pdf, HttpStatus.OK);
 	}
 }
